f13e7dc5eec6f3a8fcedd1812d0fa73933ce192c
JayantAggrwal <88024419+JayantAggrwal@users.noreply.github.com>
2023-03-28 13:05:45 +0530

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import './style.css';
+import Compo1 from './components/Compo1';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <Compo1/>
     </div>
   );
 }
---
+++ components/Compo1.tsx
@@ -0,0 +1,8 @@
+import React=require("react");
+
+function Fun()
+{
+  return <h1>Component 1</h1>
+}
+
+export default Fun;
